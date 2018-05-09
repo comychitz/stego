4a980a5a4a10587021e50d1e68b36f533ad27a39
Cameron Chitsaz <camcorder09@gmail.com>
2018-05-08 22:51:38 -0700

    made encoder into a closure

--- stego/stego.go
+++ stego/stego.go
@@ -84,16 +84,82 @@ func calcMaxMsgSize(i image.Image) int  {
      }
  }
 
-func Read(imagePath string, str *string) int {
+type Changeable interface {
+        Set(x, y int, c color.Color)
+}
+
+/**
+ * return function for encoding a character of the msg to hide into the image
+ */
+func encoder(m image.Image) func(byte, image.Image) int {
+    count := 0
+    x := m.Bounds().Min.X
+    y := m.Bounds().Min.Y
+    return func(char byte, m image.Image) int {
+        for i := uint32(0); i < 8; i++ {
+            r, g, b, a := m.At(x,y).RGBA()
+            colors := []uint32 {r,g,b}
+
+            if uint32(char) & 1<<i > 0 {
+                colors[count] = colors[count]|1
+            } else {
+                colors[count] = colors[count]&0xFFFE
+            }
+            var rgba color.RGBA
+            rgba.R = uint8(colors[0]/a)
+            rgba.G = uint8(colors[1]/a)
+            rgba.B = uint8(colors[2]/a)
+            rgba.A = uint8(a)
+
+            if img, ok := m.(Changeable); ok {
+                img.Set(x, y, rgba)
+            } else {
+                fmt.Println("Unable to modify image")
+                return 1
+            }
+            count++
+            if count > 3 {
+                // move to the next pixel
+                if x+1 > m.Bounds().Max.X {
+                    y += 1
+                    x = m.Bounds().Min.X
+                } else {
+                    x += 1
+                }
+                count = 0
+            }
+        }
+        return 0
+    }
+}
+
+func Hide(msg string, imagePath string) int {
 
     m, err := openImage(imagePath)
     if err != nil {
         fmt.Printf("Error opening image: %s\n", imagePath, err)
         return 1
     }
+    if len(msg) >= calcMaxMsgSize(m) {
+        fmt.Printf("Message (%d bytes) can't fit in this image\n", len(msg))
+        return 1
+    }
+    encode := encoder(m)
+    for _, s := range(msg) {
+        if encode(byte(s), m) != 0 {
+            return 1
+        }
+    }
+    encode(byte(ETX), m)
+    return 0
+}
 
-    // iterate over bitmap, decoding the message until "etx" value is seen
-    var msg string
+func Read(imagePath string, str *string) int {
+    m, err := openImage(imagePath)
+    if err != nil {
+        fmt.Printf("Error opening image: %s\n", imagePath, err)
+        return 1
+    }
     decode := decoder()
     Outer:
         for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
@@ -106,98 +172,11 @@ func Read(imagePath string, str *string) int {
                 if ret == ETX {
                     break Outer
                 } else if ret == CHAR {
-                    msg += string(c)
+                    *str += string(c)
                 }
             }
         }
-
-    fmt.Printf("Read hidden msg (%d chars) from image: \n%s\n", len(msg), msg)
-
-    return 0
-}
-
-type Changeable interface {
-        Set(x, y int, c color.Color)
-}
-
-/**
- * encode a character of the msg to hide into the image
- */
-var x int = -1
-var y int = -1
-var count2 int = 0
-func encode(char byte, m image.Image) int {
-
-    /*
-    if x < 0 {
-        x = m.Bounds().Min.X
-        y = m.Bounds().Min.Y
-    }
-    for i := uint32(0); i < 8; i++ {
-
-        r, g, b, a := m.At(x,y).RGBA()
-        colors := []uint32 {r,g,b}
-
-        if uint32(char) & 1<<i > 0 {
-            colors[count2] = colors[count2]|1
-        } else {
-            colors[count2] = colors[count2]&0xFFFE
-        }
-
-        var rgba color.RGBA
-        rgba.R = uint8(colors[0]/a)
-        rgba.G = uint8(colors[1]/a)
-        rgba.B = uint8(colors[2]/a)
-        rgba.A = uint8(a)
-
-        if img, ok := m.(Changeable); ok {
-            img.Set(x, y, rgba)
-        } else {
-            fmt.Println("Unable to modify image")
-            return 1
-        }
-
-        count2++
-        if count2 > 3 {
-            // move to the next pixel
-            if x+1 > m.Bounds().Max.X {
-                y += 1
-                x=0
-            } else {
-                x += 1
-            }
-            count2 = 0
-        }
-    }
-    */
-    return 0
-}
-
-func Hide(msg string, imagePath string) int {
-
-    m, err := openImage(imagePath)
-    if err != nil {
-        fmt.Printf("Error opening image: %s\n", imagePath, err)
-        return 1
-    }
-
-    maxSize := calcMaxMsgSize(m)
-    fmt.Printf("Max length of hidden message for this image: %d bytes\n", maxSize)
-
-    if len(msg) >= maxSize {
-        fmt.Printf("Message (%d bytes) can't fit in this image\n", len(msg))
-        return 1
-    }
-
-    // iterate over message, decode each byte into bits and add into image. 
-    // put "etx" ascii value  at end of msg to indicate completion
-    for _, s := range(msg) {
-        if encode(byte(s), m) != 0 {
-            return 1
-        }
-    }
-    encode(byte(ETX), m)
-
+    fmt.Printf("Read hidden msg (%d chars) from image: \n%s\n", len(*str), *str)
     return 0
 }
 
