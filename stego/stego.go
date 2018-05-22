65b02b62910beeac1cd64717dcbd193b0b0c67a8
Cameron Chitsaz <camcorder09@gmail.com>
2018-05-22 09:19:31 -0700

    some cleanup, fixes to come

--- stego/stego.go
+++ stego/stego.go
@@ -45,15 +45,26 @@ func calcMaxMsgSize(i image.Image) int  {
     return (i.Bounds().Max.X-i.Bounds().Min.X)*(i.Bounds().Max.Y-i.Bounds().Min.Y)*3/8
 }
 
+func readPixel(x int, y int, *m image.Image) (r, g, b, a uint8) {
+
+    r, g, b, a := outImg.At(x,y).RGBA()
+    r = uint8(r)
+    g = uint8(g)
+    b = uint8(b)
+    a = uint8(a)
+    fmt.Printf("Pixel [%d,%d] is: R:%d G:%d B:%d \n", x, y, r, g, b, a)
+    return r, g, b, a
+}
+
 /**
  * return a decoder for decoding the image pixel into a character
  * if a full byte hasn't been parsed yet, return PARTIAL
  */
- func decoder() func(r, g, b, a uint32, c*byte) int {
+ func decoder() func(r, g, b uint8, c*byte) int {
      var char byte = 0
      var count uint = 0
-     return func(r, g, b, a uint32, c* byte)  int {
-         colors := []uint8 {uint8(r),uint8(g),uint8(b)}
+     return func(r, g, b uint8, c* byte)  int {
+         colors := []uint8 {r, g, b}
          var leftover bool = false
          for _, c := range colors {
 
@@ -78,7 +89,6 @@ func calcMaxMsgSize(i image.Image) int  {
          count = 0
 
          if int(*c) == ETX {
-             fmt.Println("byte: ", *c)
              return ETX
          }
          fmt.Println("byte: ", *c)
@@ -90,42 +100,39 @@ func calcMaxMsgSize(i image.Image) int  {
  * return function for encoding a character of the msg to hide into the image
  */
 func encoder(m image.Image) func(byte, *image.RGBA) int {
-    count := 0
     x := m.Bounds().Min.X
     y := m.Bounds().Min.Y
+    index := uint32(0)
     return func(char byte, m *image.RGBA) int {
-        fmt.Printf("Hiding char: (%d)\n", char)
-        for i := uint32(0); i < 8; i++ {
-            r, g, b, a := m.At(x,y).RGBA()
-            colors := []uint8 {uint8(r), uint8(g), uint8(b)}
-            fmt.Printf("Read pixel [%d,%d] to: R:%d G:%d B:%d\n",
-                           x, y, colors[0], colors[1], colors[2])
+        fmt.Printf("Hiding char: %c %d %b\n", char, char, char)
+        for i := uint32(0); i < 8; i+=3 {
 
-            if uint32(char) & (1<<(7-i)) > 0 {
-                colors[count] = colors[count]|1
-            } else {
-                colors[count] = colors[count]&0xFE
+            r, g, b, a = readPixel(x, y, m)
+            colors := []uint8 {r, g, b}
+
+            for j := index; j < 3 && i+j < 8; j++ {
+                if uint32(char)&(1<<(7-(i+j))) > 0 {
+                    colors[j] = colors[j]|1
+                } else {
+                    colors[j] = colors[j]&0xFE
+                }
             }
-
             var rgba color.RGBA
             rgba.R = colors[0]
             rgba.G = colors[1]
             rgba.B = colors[2]
-            rgba.A = uint8(a)
-
-            count++
-            if count >= 3 {
-                m.SetRGBA(x, y, rgba)
-                fmt.Printf("Set pixel [%d,%d] to: R:%d G:%d B:%d\n\n",
-                           x, y, rgba.R, rgba.G, rgba.B)
-                // move to the next pixel
+            rgba.A = a
+            m.SetRGBA(x, y, rgba)
+            fmt.Printf("Set pixel [%d,%d] to: R:%d G:%d B:%d A:%d\n\n",
+                       x, y, rgba.R, rgba.G, rgba.B, rgba.A)
+            if i+j == 8 {
+                index = uint32(0)
                 if x+1 > m.Bounds().Max.X {
                     y += 1
                     x = m.Bounds().Min.X
                 } else {
                     x += 1
                 }
-                count = 0
             }
         }
         return 0
@@ -157,6 +164,9 @@ func Hide(msg string, imagePath string) int {
     }
     encode(byte(ETX), outImg)
 
+    //
+    // TODO parse image name and add -2 to end of it
+    //
     var savePath string = imagePath + "-2"
     w, err := os.Create(savePath)
     if err != nil {
@@ -169,6 +179,7 @@ func Hide(msg string, imagePath string) int {
         fmt.Printf("Error saving image (%s)\n", err)
         return 1
     }
+    fmt.Printf("Saved image %s\n", savePath)
     return 0
 }
 
@@ -178,8 +189,8 @@ func Read(imagePath string, str *string) int {
         fmt.Printf("Error opening image: %s\n", imagePath, err)
         return 1
     }
-    outImg := image.NewRGBA(m.Bounds())
-    draw.Draw(outImg, m.Bounds(), m, image.Point{}, draw.Over)
+    img := image.NewRGBA(m.Bounds())
+    draw.Draw(img, m.Bounds(), m, image.Point{}, draw.Over)
 
     decode := decoder()
     Outer:
@@ -187,15 +198,14 @@ func Read(imagePath string, str *string) int {
             for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
 
                 var c byte = 0
-                r2, g2, b2, a2 := outImg.At(x,y).RGBA()
-                fmt.Printf("Pixel [%d,%d] is: R:%d G:%d B:%d \n",
-                       x, y, uint8(r2), uint8(g2), uint8(b2))
-
-                ret := decode(r2, g2, b2, a2, &c)
+                r, g, b, _ := readPixel(x, y, img)
+                ret := decode(r, g, b, &c)
 
                 if ret == ETX {
+                    fmt.Println("got end of text: ", c)
                     break Outer
                 } else if ret == CHAR {
+                    fmt.Println("read byte: ", c)
                     *str += string(c)
                 }
             }
